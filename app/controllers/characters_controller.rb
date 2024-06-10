f5fe9257d12473db47fbcb1ad2aec41b0bf50734
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-09 23:34:11 -0400

    Show page styled, with bootstrap. Still missing to use the rating stars

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,8 @@
 class CharactersController < ApplicationController
   def show
+    @character = Character.find(params[:id])
+
+    @userevaluation = UserEvaluation.new
+    @userevaluations = @character.user_evaluations
   end
 end
--- app/controllers/user_evaluations_controller.rb
+++ app/controllers/user_evaluations_controller.rb
@@ -1,9 +1,24 @@
 class UserEvaluationsController < ApplicationController
   def new
-
+    @character = Character.find(params[:character_id])
+    @userevaluation = UserEvaluation.new
   end
 
   def create
+    @character = Character.find(params[:character_id])
+    @userevaluation = UserEvaluation.new(userevaluation_params)
+    @userevaluation.user = current_user
+    @userevaluation.character = @character
+    if @userevaluation.save
+      redirect_to character_path(@character), status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+
+  private
+  def userevaluation_params
+    params.require(:user_evaluation).permit(:comments, :userrating)
   end
 end
