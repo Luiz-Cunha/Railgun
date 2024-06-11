fb9a2a3c8623b1daddd99fefb2e9dc52ac2c0c29
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-06-10 23:29:35 -0400

    Devise is now saving the age, name and the username of the Current User

---
+++ config/initializers/devise_permitted_parameters.rb
@@ -0,0 +1,20 @@
+module DevisePermittedParameters
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :nickname])
+  end
+end
+
+Rails.application.configure do
+  config.to_prepare do
+    DeviseController.include DevisePermittedParameters
+  end
+end
